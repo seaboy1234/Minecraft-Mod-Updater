258a1090a0003d0f8a9091d6a3aebf9ac3cb4f28
Christian W <seaboy1234@rakaienguard.com>
2012-03-15 02:57:51 -0700

    Added header to files.

--- ModUpdater.Client/MainForm.cs
+++ ModUpdater.Client/MainForm.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿//    File:        MainForm.cs
+//    Copyright:   Copyright (C) 2012 Christian Wilson. All rights reserved.
+//    Website:     https://github.com/seaboy1234/Minecraft-Mod-Updater
+//    Description: This is intended to help Minecraft server owners who use mods make the experience of adding new mods and updating old ones easier for everyone.
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -109,6 +125,11 @@ namespace ModUpdater.Client
             {
                 MinecraftModUpdater.Logger.Log(Logger.Level.Warning, "Client is running in commandline.");
             }
+            TaskManager.AddAsyncTask(delegate
+            {
+                if (Extras.CheckForUpdate())
+                    UpdateForm.Open();
+            });
             if ((new ConnectionForm()).ShowDialog() != System.Windows.Forms.DialogResult.OK)
             {
                 Dispose();
--- ModUpdater/Extras.cs
+++ ModUpdater/Extras.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿//    File:        Extras.cs
+//    Copyright:   Copyright (C) 2012 Christian Wilson. All rights reserved.
+//    Website:     https://github.com/seaboy1234/Minecraft-Mod-Updater
+//    Description: This is intended to help Minecraft server owners who use mods make the experience of adding new mods and updating old ones easier for everyone.
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +22,7 @@ using System.IO;
 using System.Security.Cryptography;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Net;
 
 namespace ModUpdater
 {
@@ -68,6 +85,14 @@ namespace ModUpdater
         {
             Image i = Image.FromFile(path);
             return BytesFromImage(i);
+        }
+        public static bool CheckForUpdate()
+        {
+            WebClient c = new WebClient();
+            if (c.DownloadString("https://github.com/downloads/seaboy1234/Minecraft-Mod-Updater/relese_dev.txt") != MinecraftModUpdater.Version)
+                return true;
+            else 
+                return false;
         }
     }
 }
