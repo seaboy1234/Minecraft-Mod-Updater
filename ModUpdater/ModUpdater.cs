8f84d9714d8e2fd06d5f7650c7fcfda8b175f774
Christian W <seaboy1234@rakaienguard.com>
2012-03-17 15:57:08 -0700

    First things first.

--- ModUpdater/ModUpdater.cs
+++ ModUpdater/ModUpdater.cs
@@ -38,7 +38,7 @@ namespace ModUpdater
         /// <summary>
         /// The Mod Updater Version.
         /// </summary>
-        public const string Version = "1.2.0_rc";
+        public const string Version = "1.2.0_rc_launcher";
         /// <summary>
         /// The Logger.
         /// </summary>
