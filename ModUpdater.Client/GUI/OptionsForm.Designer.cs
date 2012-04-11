a24ac7ebd12623f974707b46591b1a9d67516028
Christian W <seaboy1234@rakaienguard.com>
2012-04-10 23:42:28 -0700

    Added an options form.

---
+++ ModUpdater.Client/GUI/OptionsForm.Designer.cs
@@ -0,0 +1,260 @@
+﻿//    File:        OptionsForm.Designer.cs
+//    Copyright:   Copyright (C) 2012 Christian Wilson. All rights reserved.
+//    Website:     https://github.com/seaboy1234/Minecraft-Mod-Updater
+//    Description: This is intended to help Minecraft server owners who use mods make the experience of adding new mods and updating old ones easier for everyone.
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+namespace ModUpdater.Client.GUI
+{
+    partial class OptionsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OptionsForm));
+            this.chkAuUpdate = new System.Windows.Forms.CheckBox();
+            this.chkStartMC = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chkRemPass = new System.Windows.Forms.CheckBox();
+            this.javaBtn = new System.Windows.Forms.Button();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtJava = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chkAuUpdate
+            // 
+            this.chkAuUpdate.AutoSize = true;
+            this.chkAuUpdate.BackColor = System.Drawing.Color.Transparent;
+            this.chkAuUpdate.ForeColor = System.Drawing.Color.White;
+            this.chkAuUpdate.Location = new System.Drawing.Point(6, 42);
+            this.chkAuUpdate.Name = "chkAuUpdate";
+            this.chkAuUpdate.Size = new System.Drawing.Size(128, 17);
+            this.chkAuUpdate.TabIndex = 10;
+            this.chkAuUpdate.Text = "Enable Auto-Update?";
+            this.chkAuUpdate.UseVisualStyleBackColor = false;
+            this.chkAuUpdate.CheckedChanged += new System.EventHandler(this.chkAuUpdate_CheckedChanged);
+            // 
+            // chkStartMC
+            // 
+            this.chkStartMC.AutoSize = true;
+            this.chkStartMC.BackColor = System.Drawing.Color.Transparent;
+            this.chkStartMC.ForeColor = System.Drawing.Color.White;
+            this.chkStartMC.Location = new System.Drawing.Point(6, 19);
+            this.chkStartMC.Name = "chkStartMC";
+            this.chkStartMC.Size = new System.Drawing.Size(159, 17);
+            this.chkStartMC.TabIndex = 9;
+            this.chkStartMC.Text = "Start Minecraft After Launch";
+            this.chkStartMC.UseVisualStyleBackColor = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.chkStartMC);
+            this.groupBox1.Controls.Add(this.chkAuUpdate);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(287, 100);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Updater Settings";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(152, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(233, 230);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.chkRemPass);
+            this.groupBox2.Controls.Add(this.javaBtn);
+            this.groupBox2.Controls.Add(this.txtUsername);
+            this.groupBox2.Controls.Add(this.txtPassword);
+            this.groupBox2.Controls.Add(this.txtJava);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.ForeColor = System.Drawing.Color.White;
+            this.groupBox2.Location = new System.Drawing.Point(12, 118);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(287, 106);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Launcher Settings";
+            // 
+            // chkRemPass
+            // 
+            this.chkRemPass.AutoSize = true;
+            this.chkRemPass.Location = new System.Drawing.Point(9, 63);
+            this.chkRemPass.Name = "chkRemPass";
+            this.chkRemPass.Size = new System.Drawing.Size(173, 17);
+            this.chkRemPass.TabIndex = 21;
+            this.chkRemPass.Text = "Remember Minecraft Password";
+            this.chkRemPass.UseVisualStyleBackColor = true;
+            // 
+            // javaBtn
+            // 
+            this.javaBtn.ForeColor = System.Drawing.Color.Black;
+            this.javaBtn.Location = new System.Drawing.Point(221, 78);
+            this.javaBtn.Name = "javaBtn";
+            this.javaBtn.Size = new System.Drawing.Size(60, 23);
+            this.javaBtn.TabIndex = 14;
+            this.javaBtn.Text = "Browse";
+            this.javaBtn.UseVisualStyleBackColor = true;
+            this.javaBtn.Click += new System.EventHandler(this.javaBtn_Click);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(114, 13);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 15;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(114, 39);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 16;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // txtJava
+            // 
+            this.txtJava.Location = new System.Drawing.Point(114, 80);
+            this.txtJava.Name = "txtJava";
+            this.txtJava.Size = new System.Drawing.Size(100, 20);
+            this.txtJava.TabIndex = 17;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Minecraft Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Minecraft Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Java Path";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // OptionsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(314, 262);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "OptionsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Options";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.OptionsForm_FormClosing);
+            this.Load += new System.EventHandler(this.OptionsForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkAuUpdate;
+        private System.Windows.Forms.CheckBox chkStartMC;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox chkRemPass;
+        private System.Windows.Forms.Button javaBtn;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtJava;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
