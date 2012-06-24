54e5c40c030e5b6be0e5f2481b0700f5b814dd88
Christian W <seaboy1234@gmail.com>
2012-06-24 01:41:34 -0700

    Working on the new logger form for the admin interface.

---
+++ ModUpdater.Admin/GUI/LoggerForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿//    File:        LoggerForm.Designer.cs
+//    Copyright:   Copyright (C) 2012 Christian Wilson. All rights reserved.
+//    Website:     https://github.com/seaboy1234/Minecraft-Mod-Updater
+//    Description: This is intended to help Minecraft server owners who use mods make the experience of adding new mods and updating old ones easier for everyone.
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+namespace ModUpdater.Admin.GUI
+{
+    partial class LoggerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(10, 12);
+            this.textBox1.TabIndex = 0;
+            // 
+            // LoggerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 321);
+            this.Controls.Add(this.textBox1);
+            this.Name = "LoggerForm";
+            this.Text = "LoggerForm";
+            this.Load += new System.EventHandler(this.LoggerForm_Load);
+            this.Resize += new System.EventHandler(this.LoggerForm_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ ModUpdater.Admin/GUI/LoggerForm.cs
@@ -0,0 +1,66 @@
+﻿//    File:        LoggerForm.cs
+//    Copyright:   Copyright (C) 2012 Christian Wilson. All rights reserved.
+//    Website:     https://github.com/seaboy1234/Minecraft-Mod-Updater
+//    Description: This is intended to help Minecraft server owners who use mods make the experience of adding new mods and updating old ones easier for everyone.
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ModUpdater.Utility;
+
+namespace ModUpdater.Admin.GUI
+{
+    public partial class LoggerForm : Form
+    {
+        public LoggerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoggerForm_Resize(object sender, EventArgs e)
+        {
+            textBox1.Size = this.ClientSize;
+        }
+
+        private void LoggerForm_Load(object sender, EventArgs e)
+        {
+            textBox1.Size = this.ClientSize;
+            MinecraftModUpdater.Logger.LogEvent += new LogEventDelegate(Logger_LogEvent);
+        }
+
+        void Logger_LogEvent(Logger.Level level, string message)
+        {
+            if (IsDisposed) return;
+            if (InvokeRequired)
+            {
+                Invoke(new LogEventDelegate(Logger_LogEvent), level, message);
+                return;
+            }
+            if ((int)level >= 0)
+            {
+                try
+                {
+                    textBox1.AppendText(string.Format("[{0}] {1}{2}", level, message, Environment.NewLine));
+                }
+                catch (InvalidOperationException) { }
+                catch (Exception e) { Program.HandleException(e); }
+            }
+        }
+    }
+}
