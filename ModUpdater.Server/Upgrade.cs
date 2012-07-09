058328b94edc8c8c12588a4af36f054932c029ed
Christian W <seaboy1234@gmail.com>
2012-07-08 20:15:43 -0700

    I seem to have forgot this when I commited the new upgrade system.

---
+++ ModUpdater.Server/Upgrade.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using ModUpdater.Utility;
+
+namespace ModUpdater.Server
+{
+    class Upgrade
+    {
+        public static void From12x()
+        {
+            Directory.CreateDirectory(Config.ModsPath + "/assets/mod");
+            foreach (string s in Directory.GetFiles(Config.ModsPath + "/ModAssets"))
+            {
+                File.Move(s, Config.ModsPath + "/assets/mod/" + Path.GetFileName(s));
+            }
+
+            MinecraftModUpdater.Logger.Log(Logger.Level.Info, "Updated server to " + Program.Version);
+        }
+    }
+}
