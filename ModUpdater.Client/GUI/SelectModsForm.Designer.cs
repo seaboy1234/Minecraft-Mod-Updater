c2fc01dee5a6c6ca357d0cb457384e45f86e7a02
Christian W <seaboy1234@gmail.com>
2012-07-11 10:33:46 -0700

    Huge progress for #63, mods now know what they require. Closes #64.

--- ModUpdater.Client/GUI/SelectModsForm.Designer.cs
+++ ModUpdater.Client/GUI/SelectModsForm.Designer.cs
@@ -30,13 +30,13 @@
         {
             this.lsUnselected = new System.Windows.Forms.ListBox();
             this.lsSelected = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnSelectAll = new System.Windows.Forms.Button();
+            this.btnUnselectAll = new System.Windows.Forms.Button();
+            this.btnUnselect = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.button5 = new System.Windows.Forms.Button();
+            this.btnDone = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lsUnselected
@@ -55,41 +55,45 @@
             this.lsSelected.Size = new System.Drawing.Size(120, 225);
             this.lsSelected.TabIndex = 1;
             // 
-            // button1
+            // btnSelect
             // 
-            this.button1.Location = new System.Drawing.Point(152, 48);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = ">";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnSelect.Location = new System.Drawing.Point(152, 48);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.TabIndex = 2;
+            this.btnSelect.Text = ">";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
             // 
-            // button2
+            // btnSelectAll
             // 
-            this.button2.Location = new System.Drawing.Point(152, 77);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = ">>";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnSelectAll.Location = new System.Drawing.Point(152, 77);
+            this.btnSelectAll.Name = "btnSelectAll";
+            this.btnSelectAll.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectAll.TabIndex = 3;
+            this.btnSelectAll.Text = ">>";
+            this.btnSelectAll.UseVisualStyleBackColor = true;
+            this.btnSelectAll.Click += new System.EventHandler(this.btnSelectAll_Click);
             // 
-            // button3
+            // btnUnselectAll
             // 
-            this.button3.Location = new System.Drawing.Point(152, 208);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "<<";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnUnselectAll.Location = new System.Drawing.Point(152, 208);
+            this.btnUnselectAll.Name = "btnUnselectAll";
+            this.btnUnselectAll.Size = new System.Drawing.Size(75, 23);
+            this.btnUnselectAll.TabIndex = 4;
+            this.btnUnselectAll.Text = "<<";
+            this.btnUnselectAll.UseVisualStyleBackColor = true;
+            this.btnUnselectAll.Click += new System.EventHandler(this.btnUnselectAll_Click);
             // 
-            // button4
+            // btnUnselect
             // 
-            this.button4.Location = new System.Drawing.Point(152, 237);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "<";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnUnselect.Location = new System.Drawing.Point(152, 237);
+            this.btnUnselect.Name = "btnUnselect";
+            this.btnUnselect.Size = new System.Drawing.Size(75, 23);
+            this.btnUnselect.TabIndex = 5;
+            this.btnUnselect.Text = "<";
+            this.btnUnselect.UseVisualStyleBackColor = true;
+            this.btnUnselect.Click += new System.EventHandler(this.btnUnselect_Click);
             // 
             // label1
             // 
@@ -109,27 +113,27 @@
             this.label2.TabIndex = 7;
             this.label2.Text = "Selected Mods";
             // 
-            // button5
+            // btnDone
             // 
-            this.button5.Location = new System.Drawing.Point(152, 138);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 8;
-            this.button5.Text = "Confirm";
-            this.button5.UseVisualStyleBackColor = true;
+            this.btnDone.Location = new System.Drawing.Point(152, 138);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(75, 23);
+            this.btnDone.TabIndex = 8;
+            this.btnDone.Text = "Confirm";
+            this.btnDone.UseVisualStyleBackColor = true;
             // 
             // SelectModsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(383, 272);
-            this.Controls.Add(this.button5);
+            this.Controls.Add(this.btnDone);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnUnselect);
+            this.Controls.Add(this.btnUnselectAll);
+            this.Controls.Add(this.btnSelectAll);
+            this.Controls.Add(this.btnSelect);
             this.Controls.Add(this.lsSelected);
             this.Controls.Add(this.lsUnselected);
             this.Name = "SelectModsForm";
@@ -143,12 +147,12 @@
 
         private System.Windows.Forms.ListBox lsUnselected;
         private System.Windows.Forms.ListBox lsSelected;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnSelectAll;
+        private System.Windows.Forms.Button btnUnselectAll;
+        private System.Windows.Forms.Button btnUnselect;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btnDone;
     }
 }
--- ModUpdater.Client/GUI/SelectModsForm.cs
+++ ModUpdater.Client/GUI/SelectModsForm.cs
@@ -6,14 +6,80 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using ModUpdater.Client.Utility;
 
 namespace ModUpdater.Client.GUI
 {
     public partial class SelectModsForm : Form
     {
+        private List<Mod> selected, unselected;
+
         public SelectModsForm()
         {
             InitializeComponent();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            Mod m = unselected.Find(((Mod)lsUnselected.SelectedItem).Identifier);
+            if (m == null)
+            {
+                MessageBox.Show("Please select a mod.");
+            }
+            SelectMod(m);
+        }
+
+        private void btnSelectAll_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUnselect_Click(object sender, EventArgs e)
+        {
+            Mod m = selected.Find(((Mod)lsSelected.SelectedItem).Identifier);
+            if (m == null)
+            {
+                MessageBox.Show("Please select a mod.");
+            }
+            UnselectMod(m);
+        }
+
+        private void btnUnselectAll_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SelectMod(Mod m)
+        {
+            foreach (Mod mod in unselected.ToArray())
+            {
+                if (m.Requires.Contains(mod.Identifier))
+                {
+                    SelectMod(mod);
+                }
+            }
+            selected.Add(m);
+            lsSelected.Items.Add(m);
+            lsUnselected.Items.Remove(m);
+            unselected.Remove(m);
+        }
+
+        private void UnselectMod(Mod m)
+        {
+            string requiredBy = "";
+            foreach (Mod mod in m.RequiredBy.ToArray())
+            {
+                if (m.RequiredBy.Contains(mod))
+                    requiredBy += mod.Name + ", ";
+            }
+            if (requiredBy != "")
+            {
+                MessageBox.Show("This mod is required by other mods! \nYou must remove " + requiredBy.Remove(requiredBy.Length - 2) + " before removing this one.");
+            }
+            selected.Remove(m);
+            lsSelected.Items.Remove(m);
+            lsUnselected.Items.Add(m);
+            unselected.Add(m);
         }
     }
 }
