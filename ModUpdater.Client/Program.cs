94e0093ef882b90844f3520836694cfd86b79af6
Christian W <seaboy1234@gmail.com>
2012-07-11 15:06:42 -0700

    Client Version 1.2.2_5 -> 1.2.2_6

--- ModUpdater.Client/Program.cs
+++ ModUpdater.Client/Program.cs
@@ -33,7 +33,7 @@ namespace ModUpdater.Client
 {
     static class Program
     {
-        public const string Version = "1.2.2_5";
+        public const string Version = "1.2.2_6";
         [DllImport("kernel32.dll")]
         private static extern int AllocConsole();
         /// <summary>
